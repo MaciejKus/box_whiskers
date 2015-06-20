c41b24b2eaaa84ab1365410aee9ea0838eec11c9
Maciej Kus <maciej.kus.925@gmail.com>
2015-06-20 12:25:44 -0700

    first

---
+++ README.hs
@@ -0,0 +1,3 @@
+a box and whiskers calculator and html5 canvas graph. Seems like there isn't a single accurate box and whiskers calc out there, so I made one. The canvas graph could be fixed up a bit, for example, moving the numeric labels around depending on the length of the number (amount of digits) etc, but it works well enough. 
+
+http://maciejkus.com/box_whiskers
